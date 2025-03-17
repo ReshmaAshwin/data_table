d919b8cd4f3d00c20ea6b5f4f6cb514491407819
reshmaashwin <reshma124@gmail.com>
2025-03-17 09:54:39 +0400

    added data table

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Payment;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class PaymentController extends Controller
+{
+    public function index(): Response
+    {
+        return Inertia::render('Payments', [
+            'payment' => Payment::all()
+        ]);
+    }
+}
